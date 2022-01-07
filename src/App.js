c7303b806f592462de450dfd19d6632b30004406
kevin-bayat <kevin.bayat@yahoo.com>
2022-01-07 18:53:14 +0330

    add react-router-dom in project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+import { Link, Route, Switch } from "react-router-dom";
 
-function App() {
+const HatePages = (props) => {
+  console.log({ props });
+
   return (
     <div>
-      <HomePage />
+      <button onClick={() => props.history.push("/hats/ggg")}>hats</button>
     </div>
   );
-}
+};
+const Hasan = (props) => {
+  console.log({ props });
+  return <div>HASAN</div>;
+};
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatePages} />
+      <Route path="/hats/ggg" component={Hasan} />
+    </Switch>
+  );
+};
 
 export default App;
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import "./directory.scss";
 import MenuItem from "../menu-item/menu-item";
 
-class Directory extends React.Component {
+class Directory extends Component {
   constructor() {
     super();
     this.state = {
