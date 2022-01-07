932c30771d0f3bdf696929733e7c8be5f424b1cf
kevin-bayat <kevin.bayat@yahoo.com>
2022-01-07 14:36:23 +0330

    complete homepage with styles

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.scss";
+import Directory from "../../components/directory/directory";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <Directory />
+    </div>
+  );
+};
+export default HomePage;
